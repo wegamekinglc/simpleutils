6e074b0fcd878d8b6de08c463660c16d7712e744
wegamekinglc <scrappedprince.li@gmail.com>
2019-11-08 12:05:22 +0800

    bump version to 0.2.0 and drop the support for python 2.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='simpleutils',
-    version='0.1.2',
+    version='0.2.0',
     packages=find_packages(),
     url='',
     license='MIT',
---
+++ simpleutils/asserts.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2019-11-8
+
+@author: cheng.li
+"""
+
+
+def require(cond: bool, exception=AssertionError, msg: str = None):
+    """
+    Deliberately defined assert function which will never be omitted by python interpreter
+
+    :param cond: bool
+    :param exception: Exception type which will be raised
+    :param msg: str
+    :return:
+    """
+    if not cond:
+        raise exception(msg)
+
+
+def is_close(x: float, y: float, absolute: float = 1e-12):
+    """
+    Check whether 2 float numbers are close enough
+
+    :param x: float
+    :param y: float
+    :param absolute: float, the threshold
+    :return: bool
+    """
+    return abs(x - y) <= absolute
